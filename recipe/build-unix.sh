e7268c4a01b2df3f5aa13d35b56292cc69913e54
Charles Bousseau <cbousseau@anaconda.com>
2026-03-10 14:38:53 -0400

    update to 4.2.0

--- recipe/build-unix.sh
+++ recipe/build-unix.sh
@@ -7,6 +7,11 @@ export CARGO_HOME=${CONDA_PREFIX}/.cargo.$(uname)
 export CARGO_CONFIG=${CARGO_HOME}/config
 export RUSTUP_HOME=${CARGO_HOME}/rustup
 
+# On macOS, reserve space so conda's install_name_tool can fix rpaths/install names later
+if [[ "$(uname)" == "Darwin" ]]; then
+  export RUSTFLAGS="${RUSTFLAGS:-} -C link-arg=-Wl,-headerpad_max_install_names"
+fi
+
 # build statically linked binary with Rust
 cargo install --verbose --locked --root "$PREFIX" --path .
 
